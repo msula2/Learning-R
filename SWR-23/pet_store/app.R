39435f679f2a69469a5d164d2a4a97d98a23f689
Hamdan Sulaiman <hamdan@7gfuse.com>
2023-10-20 17:48:47 +0500

    Changed pet store

--- SWR-23/pet_store/app.R
+++ SWR-23/pet_store/app.R
@@ -26,169 +26,43 @@ ui <- dashboardPage(
         "Homepage",
         tabName = "home_tab",
         icon = icon("house")
-      ),
-      menuItem(
-        "Dogs",
-        tabName = "dogs_tab",
-        icon = icon("dog")
-      ),
-      menuItem(
-        "Cats",
-        tabName = "cats_tab",
-        icon = icon("cat")
-      ),
-      menuItem(
-        "Birds",
-        tabName = "birds_tab",
-        icon = icon("crow")
-      ),
-      menuItem(
-        "Spiders",
-        tabName = "spiders_tab",
-        icon = icon("spider")
       )
-      
     )
   ),
   dashboardBody(
     tabItems(
       tabItem(
         tabName = "home_tab",
-        verticalLayout(
-          h2("Welcome to the pet store!"),
-          p("Our store hosts a range of pets from cute cats to scary snakes. Learn more about each pet by clicking their respective tab")
-        )
-      ),
-      tabItem(
-        tabName = "dogs_tab",
         column(
-          width = 6,
+          width = 10,
           box(
             width = 12,
-            h2("About"),
-            p("There's nothing quite like the love and excitement a puppy brings to a home. 
-              Our pet store offers a heartwarming selection of adorable puppies, each as unique as a snowflake. 
-              Whether you're looking for a spirited playmate, a devoted guardian, or a cuddly lap dog, we're here to help you find your furry soulmate.")
+            title = h3("Welcome to the pet store!"),
+            p("Our pet store allows you to create your own pet. 
+              You can choose from a range of domestic or wild animals 
+              and tinker with their appearance, personalities and traits")
           ),
           box(
             width = 12,
-            h2("Tell us what you think about them:"),
-            sliderInput("dog_slider",
-                        label = "Willngness to keep a dog as a pet",
-                        min = 0,
-                        max = 10,
-                        value = 0,
-                        step = 1,
-                        width = "100%"),
-            actionButton(
-              inputId = "submit_dog",
-              label = "Submit",
-              icon = icon("check")
+            title = "Create",
+            collapsible = TRUE,
+            status = "warning",
+            column(
+              width = 8,
+              textInput("name", label = "What is your pet's name?", value = "", placeholder = "Name", width = "100%"),
+              selectInput("type", "Choose type of animal", c("Cat", "Dog", "Bird", "Spider", "Snake"), width = "100%"),
+              actionButton(
+                inputId = "create",
+                label = "Create",
+                icon = icon("plus")
+              )
+            ),
+            column(
+              width = 4,
+              imageOutput("pet_image")
             )
+            
           )
-        ),
-        column(
-          width = 6,
-          plotOutput("sliderPlotDog")
-          
-        )
-        
-      ),
-      tabItem(
-        tabName = "cats_tab",
-        column(
-          width = 6,
-          box(
-            width = 12,
-            h2("About"),
-            p("Cats, known for their grace, independence, and charm, bring joy to countless households around the world.
-              With their soft purrs and gentle meows, they effortlessly melt our hearts.")
-          ),
-          box(
-            width = 12,
-            h2("Tell us what you think about them:"),
-            sliderInput("cat_slider",
-                        label = "Willngness to keep a cat as a pet",
-                        min = 0,
-                        max = 10,
-                        value = 0,
-                        step = 1,
-                        width = "100%"),
-            actionButton(
-              inputId = "submit_cat",
-              label = "Submit",
-              icon = icon("check")
-            )
-          )
-        ),
-        column(
-          width = 6,
-          plotOutput("sliderPlotCat")
-          
-        )
-      ),
-      tabItem(
-        tabName = "birds_tab",
-        column(
-          width = 6,
-          box(
-            width = 12,
-            h2("About"),
-            p("From the sweet melodies of canaries to the intelligent antics of parrots, our diverse selection of birds represents the beauty and diversity of the avian world. 
-              Our mission is to connect these wonderful feathered friends with loving homes, where they can bring joy, laughter, and a touch of nature's wonder to your life.")
-          ),
-          box(
-            width = 12,
-            h2("Tell us what you think about them:"),
-            sliderInput("bird_slider",
-                        label = "Willngness to keep a bird as a pet",
-                        min = 0,
-                        max = 10,
-                        value = 0,
-                        step = 1,
-                        width = "100%"),
-            actionButton(
-              inputId = "submit_bird",
-              label = "Submit",
-              icon = icon("check")
-            )
-          )
-        ),
-        column(
-          width = 6,
-          plotOutput("sliderPlotBird")
-          
-        )
-      ),
-      tabItem(
-        tabName = "spiders_tab",
-        column(
-          width = 6,
-          box(
-            width = 12,
-            h2("About"),
-            p("Arachnids have long been shrouded in myths and legends, but their unique allure and distinct beauty make them a truly extraordinary choice for pet enthusiasts")
-          ),
-          box(
-            width = 12,
-            h2("Tell us what you think about them:"),
-            sliderInput("spider_slider",
-                        label = "Willngness to keep a spider as a pet",
-                        min = 0,
-                        max = 10,
-                        value = 0,
-                        step = 1,
-                        width = "100%"),
-            actionButton(
-              inputId = "submit_spider",
-              label = "Submit",
-              icon = icon("check")
-            )
-          )
-        ),
-        column(
-          width = 6,
-          plotOutput("sliderPlotSpider")
           
         )
       )
@@ -198,75 +72,29 @@ ui <- dashboardPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  slider <- reactiveValues(vals = c(0,0,0,0), labels = c("Dogs", "Cats", "Spiders", "Birds"))
-  
-  observeEvent(input$submit_dog, {
-    selected_index <- which(slider$labels == "Dogs")
-    if (length(selected_index) > 0) {
-      slider$vals[selected_index] <- input$dog_slider
-    }
-    data <- data.frame(Name = slider$labels,
-                       Score = slider$vals)
+  v <- reactiveValues(
+    data = data.frame(Name = character(), Type = factor(), Index = numeric()),
+    count = 1
+  )
+  observeEvent(input$create, {
+    new_row <- data.frame(
+      Name = input$name,
+      Type = input$type,
+      Index = v$count
+    )
+    v$data <- rbind(v$data, new_row)
+    v$count <- v$count + 1
+    print(v$data)
     
-    output$sliderPlotDog <- renderPlot({
-      ggplot(data, aes(x = Name, y = Score)) +
-        geom_bar(stat = "identity", fill = "skyblue") +
-        labs(title = "Willingness to keep pet", x = "Name", y = "Willingness") +
-        scale_y_continuous(breaks = seq(0, 10, by = 1)) +
-        theme_classic()
-    })
   })
-    
-  observeEvent(input$submit_cat, {
-    selected_index <- which(slider$labels == "Cats")
-    if (length(selected_index) > 0) {
-      slider$vals[selected_index] <- input$cat_slider
-    }
-    data <- data.frame(Name = slider$labels,
-                       Score = slider$vals)
-    
-    output$sliderPlotCat <- renderPlot({
-      ggplot(data, aes(x = Name, y = Score)) +
-        geom_bar(stat = "identity", fill = "skyblue") +
-        labs(title = "Willingness to keep pet", x = "Name", y = "Willingness") +
-        scale_y_continuous(breaks = seq(0, 10, by = 1)) +
-        theme_classic()
-    })
-  })
-  
-  observeEvent(input$submit_bird, {
-    selected_index <- which(slider$labels == "Birds")
-    if (length(selected_index) > 0) {
-      slider$vals[selected_index] <- input$bird_slider
-    }
-    data <- data.frame(Name = slider$labels,
-                       Score = slider$vals)
-    
-    output$sliderPlotBird <- renderPlot({
-      ggplot(data, aes(x = Name, y = Score)) +
-        geom_bar(stat = "identity", fill = "skyblue") +
-        labs(title = "Willingness to keep pet", x = "Name", y = "Willingness") +
-        scale_y_continuous(breaks = seq(0, 10, by = 1)) +
-        theme_classic()
-    })
-  })
-  
-  observeEvent(input$submit_spider, {
-    selected_index <- which(slider$labels == "Spiders")
-    if (length(selected_index) > 0) {
-      slider$vals[selected_index] <- input$spider_slider
-    }
-    data <- data.frame(Name = slider$labels,
-                       Score = slider$vals)
-    
-    output$sliderPlotSpider <- renderPlot({
-      ggplot(data, aes(x = Name, y = Score)) +
-        geom_bar(stat = "identity", fill = "skyblue") +
-        labs(title = "Willingness to keep pet", x = "Name", y = "Willingness") +
-        scale_y_continuous(breaks = seq(0, 10, by = 1)) +
-        theme_classic()
-    })
-  })
+  output$pet_image <- renderImage({
+    image_path <- paste("images/", ".jpg", sep = input$type)
+    list(src = image_path,
+         width = 150,
+         height = 150,
+         alt = "A pet")
+  }, deleteFile = FALSE)
+
   
 }
 
