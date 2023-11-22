6c583c679b0646786e7b984f63fb73578f11bd06
Hamdan Sulaiman <hamdan@7gfuse.com>
2023-11-22 12:53:04 +0500

    Removed katex

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 # Install and load the shiny package if you haven't already
 # install.packages("shiny")
 # library(shiny)
-library(katex)
 # Define the UI for the app
 ui <- fluidPage(
   titlePanel("Simple Shiny App"),
